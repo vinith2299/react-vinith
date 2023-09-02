d190ff3f03b23cf4b7e2c0896a540bc730aa488b
vinith2299 <141495701+vinith2299@users.noreply.github.com>
2023-09-02 12:46:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-vinith
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-lt1qju)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello vinith!</h1>
+      <p>Start editing to see some magic happen hello:)</p>
+    </div>
+  );
+}
